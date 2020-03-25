d63ef0bdd81546c874abbaacf2665b20edec0aa6
David Isakson <ikeman32@yahoo.com>
2020-03-25 12:51:03 -0700

    mvp update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 3300;
+const port = process.env.PORT || 3400;
 server.listen(port, () => console.log(`\n** Running on port port ${port} **\n`));
